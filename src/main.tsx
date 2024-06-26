f119e470c21830260de0399b1d992c45f6f194cd
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-26 17:31:06 +0200

    add tanstack react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {RouterProvider, createRouter} from '@tanstack/react-router';
+import {routeTree} from './routeTree.gen';
 import './index.css';
-import App from './app';
+
+const router = createRouter({routeTree});
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>,
   );
 } else {

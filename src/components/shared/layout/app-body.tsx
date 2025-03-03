aad656266a5adf1faa79959acf2de35b69030073
eduard-cc <eduard.ciociu22@gmail.com>
2025-03-03 18:45:56 +0100

    add back button to layout

--- src/components/shared/layout/app-body.tsx
+++ src/components/shared/layout/app-body.tsx
@@ -1,7 +1,29 @@
+import {useCanGoBack, useRouter} from '@tanstack/react-router';
+import {ChevronLeft} from 'lucide-react';
+
+import {Button} from '@/components/ui/button';
+
 type AppBodyProps = {
   children: React.ReactNode;
 };
 
 export function AppBody({children}: AppBodyProps) {
-  return <div className='mx-auto max-w-[80rem] px-6'>{children}</div>;
+  const router = useRouter();
+  const canGoBack = useCanGoBack();
+
+  return (
+    <div className='mx-auto max-w-[80rem] px-6'>
+      {canGoBack && (
+        <Button
+          variant='link'
+          onClick={() => router.history.back()}
+          className='-ml-4 mt-4 text-foreground'
+        >
+          <ChevronLeft />
+          Back
+        </Button>
+      )}
+      {children}
+    </div>
+  );
 }

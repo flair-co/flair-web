8cb9a2f15f785e29b6a2d86713335b163a6555dc
eduard-cc <eduard.ciociu22@gmail.com>
2025-03-04 00:06:44 +0100

    move back-button.tsx out of body layout

--- src/components/shared/layout/app-body.tsx
+++ src/components/shared/layout/app-body.tsx
@@ -1,29 +1,7 @@
-import {useCanGoBack, useRouter} from '@tanstack/react-router';
-import {ChevronLeft} from 'lucide-react';
-
-import {Button} from '@/components/ui/button';
-
 type AppBodyProps = {
   children: React.ReactNode;
 };
 
 export function AppBody({children}: AppBodyProps) {
-  const router = useRouter();
-  const canGoBack = useCanGoBack();
-
-  return (
-    <div className='mx-auto max-w-[80rem] px-6'>
-      {canGoBack && (
-        <Button
-          variant='link'
-          onClick={() => router.history.back()}
-          className='-ml-4 mt-4 text-foreground'
-        >
-          <ChevronLeft />
-          Back
-        </Button>
-      )}
-      {children}
-    </div>
-  );
+  return <div className='mx-auto max-w-[80rem] px-6'>{children}</div>;
 }

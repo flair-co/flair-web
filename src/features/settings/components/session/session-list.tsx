01734aee52207744bfcd2ff7bedee99e4dcb7945
eduard-cc <eduard.ciociu22@gmail.com>
2025-05-01 02:31:34 +0200

    refactor sessions ui

--- src/features/settings/components/session/session-list.tsx
+++ src/features/settings/components/session/session-list.tsx
@@ -7,22 +7,14 @@ export function SessionList() {
   const {sessions, isPending} = useGetSessions();
 
   return (
-    <div className='mt-4'>
-      <div className='flex flex-col items-start justify-between sm:flex-row sm:items-center'>
-        <div>
-          <h2 className='mb-1 text-lg font-medium'>Sessions</h2>
-          <p className='mr-8 text-sm text-muted-foreground'>Devices logged into your account.</p>
-        </div>
+    <div className='mt-8'>
+      <div className='mb-4'>
+        <h2 className='mb-1 text-lg font-medium'>Sessions</h2>
+        <p className='mr-8 text-sm text-muted-foreground'>Devices logged into your account.</p>
       </div>
       <div className='mt-4 space-y-4'>
-        {isPending && (
-          <>
-            <Skeleton className='h-[4.5rem] w-full rounded-lg bg-muted' />
-          </>
-        )}
-        {sessions &&
-          sessions.length > 0 &&
-          sessions.map((session) => <SessionCard key={session.id} session={session} />)}
+        {isPending && <Skeleton className='h-[4.5rem] w-full rounded-lg bg-muted' />}
+        {sessions && sessions.map((session) => <SessionCard key={session.id} session={session} />)}
       </div>
     </div>
   );

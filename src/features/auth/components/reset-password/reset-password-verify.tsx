daf92aece5a8d816bd9c955f14fb851366ad31ca
eduard-cc <eduard.ciociu22@gmail.com>
2025-05-05 02:33:21 +0200

    add success state

--- src/features/auth/components/reset-password/reset-password-verify.tsx
+++ src/features/auth/components/reset-password/reset-password-verify.tsx
@@ -31,66 +31,82 @@ export function ResetPasswordVerify({token, email}: ResetPasswordVerifyProps) {
     defaultValues: {token},
   });
 
-  const {verifyPasswordReset, isPending} = usePasswordResetVerify();
+  const {verifyPasswordReset, isPending, isSuccess} = usePasswordResetVerify();
 
   async function onSubmit(formData: PasswordResetVerifyDto) {
     await verifyPasswordReset(formData);
   }
 
   return (
-    <AuthLayout title='Reset password'>
+    <AuthLayout title={isSuccess ? 'Password reset successful' : 'Reset password'}>
       <div className='relative flex flex-col'>
         <AnimatePresence initial={false} mode='wait'>
-          <motion.div
-            key='verify-form-view-fade'
-            variants={switchContentVariants}
-            initial='hidden'
-            animate='visible'
-            exit='exit'
-            className='flex w-full flex-col space-y-4'
-          >
-            <p className='text-sm text-muted-foreground'>
-              Set a new password for the account associated with{' '}
-              <span className='text-foreground'>{email}</span>.
-            </p>
-            <Form {...form}>
-              <form
-                className={cn('grid items-start gap-4')}
-                onSubmit={form.handleSubmit(onSubmit)}
-                noValidate
-              >
-                <FormField
-                  control={form.control}
-                  name='newPassword'
-                  render={({field, fieldState}) => (
-                    <PasswordInputField<PasswordResetVerifyDto, 'newPassword'>
-                      field={field}
-                      fieldState={fieldState}
-                      label='New password'
-                      id='new-password'
-                      autoComplete='new-password'
-                      disabled={isPending}
-                    />
-                  )}
-                />
-                <Button type='submit' disabled={isPending} className='mt-2'>
-                  {isPending ? (
-                    <>
-                      <span>Resetting password...</span>
-                      <Loader className='ml-2 h-4 w-4 animate-slow-spin' />
-                    </>
-                  ) : (
-                    <span>Reset password</span>
-                  )}
-                </Button>
-              </form>
-            </Form>
-            <p className='px-8 pt-4 text-center text-sm text-muted-foreground'>
-              <Link to='/login' className='text-foreground underline-offset-4 hover:underline'>
-                Return to log in
-              </Link>
-            </p>
-          </motion.div>
+          {isSuccess ? (
+            <motion.div
+              key='success-view-fade'
+              variants={switchContentVariants}
+              initial='hidden'
+              animate='visible'
+              exit='exit'
+              className='flex w-full flex-col space-y-4 text-sm text-muted-foreground'
+            >
+              <p className='mb-2'>You can now use your new password to log in to your account.</p>
+              <Button asChild>
+                <Link to='/login'>Go to log in</Link>
+              </Button>
+            </motion.div>
+          ) : (
+            <motion.div
+              key='verify-form-view-fade'
+              variants={switchContentVariants}
+              initial='hidden'
+              animate='visible'
+              exit='exit'
+              className='flex w-full flex-col space-y-4'
+            >
+              <p className='text-sm text-muted-foreground'>
+                Set a new password for the account associated with{' '}
+                <span className='text-foreground'>{email}</span>.
+              </p>
+              <Form {...form}>
+                <form
+                  className={cn('grid items-start gap-4')}
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  noValidate
+                >
+                  <FormField
+                    control={form.control}
+                    name='newPassword'
+                    render={({field, fieldState}) => (
+                      <PasswordInputField<PasswordResetVerifyDto, 'newPassword'>
+                        field={field}
+                        fieldState={fieldState}
+                        label='New password'
+                        id='new-password'
+                        autoComplete='new-password'
+                        disabled={isPending}
+                      />
+                    )}
+                  />
+                  <Button type='submit' disabled={isPending} className='mt-2'>
+                    {isPending ? (
+                      <>
+                        <span>Resetting password...</span>
+                        <Loader className='ml-2 h-4 w-4 animate-slow-spin' />
+                      </>
+                    ) : (
+                      <span>Reset password</span>
+                    )}
+                  </Button>
+                </form>
+              </Form>
+              <p className='px-8 pt-4 text-center text-sm text-muted-foreground'>
+                <Link to='/login' className='text-foreground underline-offset-4 hover:underline'>
+                  Return to log in
+                </Link>
+              </p>
+            </motion.div>
+          )}
         </AnimatePresence>
       </div>
     </AuthLayout>

904e0c0df566bd6c96bf645eaae365513c0510be
eduard-cc <eduard.ciociu22@gmail.com>
2025-06-24 21:30:16 +0200

    improve performance of currency list using tanstack/react-virtual

--- src/features/bank-account/components/bank-account-add/currency-list.tsx
+++ src/features/bank-account/components/bank-account-add/currency-list.tsx
@@ -1,5 +1,6 @@
+import {useVirtualizer} from '@tanstack/react-virtual';
 import {Check} from 'lucide-react';
-import {useMemo} from 'react';
+import {useEffect, useMemo, useRef, useState} from 'react';
 
 import {
   Command,
@@ -28,36 +29,78 @@ export function CurrencyList({
   setSelectedCurrency,
   selectedCurrency,
 }: CurrencyListProps) {
+  const [query, setQuery] = useState<string>('');
+
   const placeholder = useMemo(() => `Search ${currencies?.length} supported currencies...`, []);
+
+  const filteredCurrencies = useMemo(() => {
+    if (!currencies) return [];
+    return currencies.filter(
+      (c) =>
+        c.name.toLowerCase().includes(query.toLowerCase()) ||
+        c.code.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [currencies, query]);
+
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  const rowVirtualizer = useVirtualizer({
+    count: filteredCurrencies.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 52,
+    overscan: 5,
+  });
+
+  useEffect(() => {
+    rowVirtualizer.measure();
+  }, [filteredCurrencies, rowVirtualizer]);
 
   return (
     <Command>
-      <CommandInput placeholder={placeholder} disabled={!currencies} />
-      <CommandList className='max-h-[250px]'>
+      <CommandInput
+        placeholder={placeholder}
+        disabled={!currencies}
+        value={query}
+        onValueChange={setQuery}
+      />
+      <CommandList>
         <CommandEmpty>No supported currencies found.</CommandEmpty>
         <ScrollArea onWheel={(e) => e.stopPropagation()}>
-          <div className='max-h-[250px]'>
-            <CommandGroup>
-              {currencies &&
-                currencies.map((currency) => (
-                  <CommandItem
-                    key={currency.code}
-                    value={currency.name}
-                    className='mr-2 flex items-center justify-between'
-                    onSelect={() => {
-                      setSelectedCurrency(currency);
-                      setOpen(false);
-                    }}
-                  >
-                    <CurrencyDisplay currency={currency} className='pl-1' />
-                    <Check
-                      className={cn(
-                        'ml-2 h-4 w-4',
-                        selectedCurrency === currency.code ? 'opacity-100' : 'opacity-0',
-                      )}
-                    />
-                  </CommandItem>
-                ))}
+          <div className='max-h-[250px] overflow-auto' ref={parentRef}>
+            <CommandGroup
+              style={{height: `${rowVirtualizer.getTotalSize()}px`, position: 'relative'}}
+            >
+              {filteredCurrencies &&
+                rowVirtualizer.getVirtualItems().map((virtualItem) => {
+                  const currency = filteredCurrencies[virtualItem.index];
+                  return (
+                    <CommandItem
+                      key={currency.code}
+                      value={currency.name}
+                      className='mr-2 flex items-center justify-between'
+                      style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: `${virtualItem.size}px`,
+                        transform: `translateY(${virtualItem.start}px)`,
+                      }}
+                      onSelect={() => {
+                        setSelectedCurrency(currency);
+                        setOpen(false);
+                      }}
+                    >
+                      <CurrencyDisplay currency={currency} className='pl-1' />
+                      <Check
+                        className={cn(
+                          'ml-2 h-4 w-4',
+                          selectedCurrency === currency.code ? 'opacity-100' : 'opacity-0',
+                        )}
+                      />
+                    </CommandItem>
+                  );
+                })}
             </CommandGroup>
           </div>
         </ScrollArea>
